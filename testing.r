a64abc2fca9c57d6798efa2b75ce4443a76a9bbf
Iqbal Khan <100311281+IqbalKhaan@users.noreply.github.com>
2022-02-28 00:19:14 +0500

    testing.r
    basic steps

---
+++ testing.r
@@ -0,0 +1,9 @@
+#This is the first step, I created a new repository by the the '+' sign at the top left corner.
+# I gave name to the repository hello-r
+# Now I am adding a '.r' file, beause I will call this file in R., you can created file in any programming like, C, HTML etc.
+# for this I click on 'create a new file' and give it a name (testing.r)
+# I am creating a file here.
+# Once you have done, commit this file, in the commit section we will give name to the file, and add description if you want.
+finally click on 'commit new file' and we are don.
+
+print('Basic Steps in R')
